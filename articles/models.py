7fd3bfaee4cc37f88926a9312499673c408f4d1d
tmsnvl95 <tmsnvl95@naver.com>
2023-01-18 22:48:40 +0900

    model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Ask(models.Model):
+    question = models.TextField()
+    answer1 = models.TextField()
+    answer2 = models.TextField()
+    alphabet = models.BooleanField(default=False) 
+
+
+class Mbti(models.Model):
+    alphabet = models.CharField()
+    hits = models.PositiveIntegerField(default=0)
+
+
+class Type(models.Model):
+    person = models.CharField() 
+    verse = models.TextField()
+    mbti =  models.CharField()
+    
