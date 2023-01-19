daeb1a378ff96894e1a27d21a9ddeb9581f99d18
kklee0930 <kklee0930@gmail.com>
2023-01-19 22:09:35 +0900

    [Backend] 모델 수정, url 설정 완료

--- articles/models.py
+++ articles/models.py
@@ -2,20 +2,25 @@ from django.db import models
 
 # Create your models here.
 
+# 질문 모델
 class Ask(models.Model):
-    question = models.TextField()
-    answer1 = models.TextField()
-    answer2 = models.TextField()
+    question = models.TextField(max_length=100)
+    answer1 = models.TextField(max_length=100)
+    answer2 = models.TextField(max_length=100)
     alphabet = models.BooleanField(default=False) 
 
-
+# mbti 모델 (alphabet컬럼에 선택지? 전부 받아오기)
+# hits 필요 없을듯?
 class Mbti(models.Model):
-    alphabet = models.CharField()
+    alphabet = models.CharField(max_length=100)
     hits = models.PositiveIntegerField(default=0)
 
-
+# 인물 모델
 class Type(models.Model):
-    person = models.CharField() 
-    verse = models.TextField()
-    mbti =  models.CharField()
+    person = models.CharField(max_length=100) 
+    verse = models.TextField(max_length=100)
+    mbti =  models.CharField(max_length=100)
+    
+    
+    
     
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from .models import Mbti
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'articles/index.html', context)
+
+def result(request):
+    mbti = Mbti().alphabet
+    
+    first = {}
+    second = {}
+    third = {}
+    fourth = {}
+    result = ''
+    
+    first['I'] = mbti.count('I')
+    first['E'] = mbti.count('E')
+    result += max(first.keys())
+    
+    second['S'] = mbti.count('S')
+    second['N'] = mbti.count('N')
+    result += max(second.keys())
+    
+    third['T'] = mbti.count('T')
+    third['F'] = mbti.count('F')
+    result += max(third.keys())
+
+    fourth['P'] = mbti.count('P')
+    fourth['J'] = mbti.count('J')
+    result += max(fourth.keys())
+    
+    context = {
+        'result': result,
+    }
+    
+    return render(request, 'articles/result.html', context)
