cc5255c1d81fb6743a41e127fa6c899f97e930a4
kklee0930 <kklee0930@gmail.com>
2023-02-02 22:48:36 +0900

    배포테스트

--- config/settings.py
+++ config/settings.py
@@ -142,14 +142,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"
 
 if DEBUG == "True": 
-    # MEDIA_URL = "/media/"
-    # MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "media"
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -158,6 +157,8 @@ if DEBUG == "True":
     }
 
 else:   
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
@@ -175,6 +176,6 @@ else:
             "USER": "postgres",
             "PASSWORD": os.getenv("DATABASE_PASSWORD"), # .env 파일에 value 작성
             "HOST": os.getenv("DATABASE_HOST"), # .env 파일에 value 작성
-            "PORT": "5432",
+            "PORT": "5433",
         }
     }
